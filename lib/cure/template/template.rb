6a1e0cfb09cd37cceca9bba8a6afc2ea7699e325
william <me@williamthom.as>
2022-09-18 21:38:13 +1000

    wip: export sections if multiple are defined

--- lib/cure/template/template.rb
+++ lib/cure/template/template.rb
@@ -11,12 +11,16 @@ module Cure
     # @param [Cure::Extraction] extraction
     attr_accessor :extraction
 
+    # @param [Cure::Export] export
+    attr_accessor :export
+
     # @param [Hash] hash
     # @return [Cure::Template]
     def self.from_hash(hash)
       this = Cure::Template.new
       this.transformations = Cure::Transformations.from_hash(hash.fetch("transformations", {}))
       this.extraction = Cure::Extraction.from_hash(hash.fetch("extraction", {}))
+      this.export = Cure::Extraction.from_hash(hash.fetch("export", {}))
       this
     end
   end
