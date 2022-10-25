7ebd3106def3adc1fbf0c8474b0db7b4c61b0d7b
william <me@williamthom.as>
2022-10-25 22:02:10 +1000

    wip: continued work on lexer.rb

--- lib/cure/eval/lexer.rb
+++ lib/cure/eval/lexer.rb
@@ -43,6 +43,11 @@ module Cure
           advance
         end
 
+        # If we find an identifier we scan thru until it isn't
+        return scan_identifier if identifier_start? @peek
+
+        return scan_number if number? @peek
+
         @peek
       end
 
@@ -57,6 +62,86 @@ module Cure
 
       private
 
+      # @return [Cure::Eval::Token]
+      def scan_identifier
+        idx = @scanner.pos
+        chars = []
+
+        while identifier_start? @peek
+          chars << @peek
+          advance
+        end
+
+        Token.new(idx, chars.join)
+      end
+
+      def scan_number
+        idx = @scanner.pos
+        nums = []
+
+        is_integer = true
+
+        loop do
+          unless number?(@peek)
+            if @peek == PERIOD
+              is_integer = false
+            elsif e_notation_start? @peek
+              nums << @peek
+              advance
+
+              if e_notation_sign? @peek
+                nums << @peek
+                advance
+              end
+
+              raise "Invalid E notation" unless number? @peek
+
+              is_integer = false
+            else
+              break
+            end
+          end
+
+          nums << @peek
+          advance
+        end
+
+        number_str = nums.join
+        Token.new(idx, is_integer ? Integer(number_str) : Float(number_str))
+      end
+
+      # @param [String, Integer] char
+      # @return [TrueClass, FalseClass]
+      def identifier_start?(char)
+        char_ord = char.ord
+        (LOWER_A <= char_ord && char_ord <= LOWER_Z) ||
+          (UPPER_A <= char_ord && char_ord <= UPPER_Z) ||
+          (char_ord == UNDERSCORE) ||
+          (char_ord == DOLLAR)
+      end
+
+      # @param [String, Integer] char
+      # @return [TrueClass, FalseClass]
+      def number?(char)
+        char_ord = char.ord
+        (NUM_ZERO <= char_ord && char_ord <= NUM_NINE)
+      end
+
+      # @param [String, Integer] char
+      # @return [TrueClass, FalseClass]
+      def e_notation_start?(char)
+        char_ord = char.ord
+        [LOWER_E, UPPER_E].include?(char_ord)
+      end
+
+      # @param [String, Integer] char
+      # @return [TrueClass, FalseClass]
+      def e_notation_sign?(char)
+        char_ord = char.ord
+        [MINUS, PLUS].include?(char_ord)
+      end
+
+      # @return [String (frozen)]
       def print_current_peek
         "#{@peek.ord} [#{@peek}]"
       end
@@ -64,6 +149,23 @@ module Cure
       EOF = -1
       SPACE = 32
 
+      DOLLAR = 36
+      UNDERSCORE = 94
+      PLUS = 43
+      MINUS = 45
+      PERIOD = 46
+
+
+      LOWER_A = 65
+      LOWER_E = 101
+      LOWER_Z = 122
+
+      UPPER_A = 65
+      UPPER_E = 69
+      UPPER_Z = 90
+
+      NUM_ZERO = 48
+      NUM_NINE = 57
     end
 
     class Token
--- spec/cure/eval/lexer_spec.rb
+++ spec/cure/eval/lexer_spec.rb
@@ -5,9 +5,31 @@ require "cure/eval/lexer"
 RSpec.describe Cure::Eval::Lexer do
   describe ".lex" do
     it "will ignore white space" do
-      tokens = described_class.lex(" 44 +   3")
+      tokens = described_class.lex(" 12 +   3")
       puts tokens
-      expect(tokens).to eq(%w[4 4 + 3])
+      # expect(tokens).to eq(%w[1 2 + 3])
+    end
+
+
+    it "will find identity" do
+      tokens = described_class.lex("identity + 123e1")
+      puts tokens
+      # expect(tokens).to eq(%w[identity + 123e1])
+    end
+  end
+end
+
+RSpec.describe Cure::Eval::Scanner do
+  describe "#identifier_start?" do
+
+    it "will return true if identity" do
+      result = described_class.new("text").send(:identifier_start?, "a")
+      expect(result).to be_truthy
+    end
+
+    it "will return false if not identity" do
+      result = described_class.new("text").send(:identifier_start?, "*")
+      expect(result).to be_falsey
     end
   end
 end
