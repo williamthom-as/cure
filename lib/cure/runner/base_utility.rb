d28d1e21e7b227be73c04a7360f9267607ee52d6
william <me@williamthom.as>
2024-03-16 21:03:55 +1000

    task: add proc-like handler to base utility

--- lib/cure/runner/base_utility.rb
+++ lib/cure/runner/base_utility.rb
@@ -5,6 +5,12 @@ module Cure
 
     # @param [String] _source
     # @param [Cure::Transforms::RowCtx] _ctx
+    def self.call(source, _ctx)
+      new.call(source, _ctx)
+    end
+
+    # @param [String] _source
+    # @param [Cure::Transforms::RowCtx] _ctx
     def call(_source, _ctx)
       raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
     end
