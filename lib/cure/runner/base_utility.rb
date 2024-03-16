0b577bf297ac39b783a61e571e76120b06b2f4cb
william <me@williamthom.as>
2024-03-16 20:49:42 +1000

    feat: add base utility class to hang off other functions

---
+++ lib/cure/runner/base_utility.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Cure
+  class BaseUtility
+
+    # @param [String] _source
+    # @param [Cure::Transforms::RowCtx] _ctx
+    def call(_source, _ctx)
+      raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+    end
+  end
+end
